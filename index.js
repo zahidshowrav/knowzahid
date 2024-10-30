bc391d16ee52ef2917757b5b5e1c93020a460ea3
Zahid R. Showrav <zahid.showravbd@gmail.com>
2024-10-31 01:28:33 +0600

    Added the cli output

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+console.log(`
+    Zahid R. Showrav
+
+    JavaScript Enthusiast and Full-Stack Developer.
+    Skilled in React, Next.js, Node.js, TypeScript, Vue, Remix, Tailwind, and Docker. 
+    Off the keyboard, he's all about music, travel, and food adventures.
+
+    📧 Email:     zahid.showravbd@gmail.com
+    🌐 Portfolio: https://zahidshowrav.me
+    ⭐ Github:    https://github.com/zahidshowrav
+    🔗 LinkedIn:  https://www.linkedin.com/in/zahidshowrav/
+    ❌ X:         https://x.com/zahidshowrav
+    📖 Facebook:  https://facebook.com/zahidshowrav
+`);
